45a2185a46b9df5082b67dde63b9deead5afe2df
Abhishek-vk <abhishek.vk100@gmail.com>
2021-05-22 18:20:37 +0530

    Updates

---
+++ precache-manifest.31e40490fe208a5a7458e7b9a92896f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ceaf6672f2b90963da214a42aba5a0",
+    "url": "/TIC-TAC-TOE/index.html"
+  },
+  {
+    "revision": "79ee0506df91048da76a",
+    "url": "/TIC-TAC-TOE/static/css/main.38cd76b4.chunk.css"
+  },
+  {
+    "revision": "1eb01c23c055b485c80a",
+    "url": "/TIC-TAC-TOE/static/js/2.2f6ec1d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TIC-TAC-TOE/static/js/2.2f6ec1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ee0506df91048da76a",
+    "url": "/TIC-TAC-TOE/static/js/main.14941082.chunk.js"
+  },
+  {
+    "revision": "c90d7d5224292ff1f92c",
+    "url": "/TIC-TAC-TOE/static/js/runtime-main.c630cdf8.js"
+  }
+]);
