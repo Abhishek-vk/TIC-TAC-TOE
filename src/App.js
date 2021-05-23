b42a828d130549f9e9d8910fd878239b7dc17530
Abhishek-vk <abhishek.vk100@gmail.com>
2021-05-23 14:40:06 +0530

    footer update

--- src/App.js
+++ src/App.js
@@ -132,10 +132,8 @@ class App extends React.Component{
     return(
       <div className='App'>
       <header><h4>TIC-TAC-TOE</h4></header>
-      <div id="content">
-	      <Board value={value} current={current} setValue={()=>this.setValue(value,current)} count={count}/>
-	      <button className='btn-reset' onClick={this.resetValue}>RESET</button>
-      </div>
+      <Board value={value} current={current} setValue={()=>this.setValue(value,current)} count={count}/>
+      <button className='btn-reset' onClick={this.resetValue}>RESET</button>
       <footer></footer>
       </div>
     )
